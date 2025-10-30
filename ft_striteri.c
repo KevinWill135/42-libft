5d7f9315d2c4cdae0d0f2b8559350ff76f31236f
Kevin De Paula <kde-paul@c1r5s11.42lisboa.com>
2025-10-30 22:10:38 +0000

    I just finished the striteri test

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: kde-paul <kde-paul@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 16:51:05 by kde-paul          #+#    #+#             */
-/*   Updated: 2025/10/20 21:22:41 by kde-paul         ###   ########.fr       */
+/*   Updated: 2025/10/30 20:21:22 by kde-paul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,26 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 		f(i, &s[i]);
 		i++;
 	}
+}
+
+#include <stdio.h>
+
+void	ft_changecase(unsigned int i, char *s)
+{
+	printf("char: %c\n", s[i]);
+	if (ft_isalpha(s[i]))
+	{
+		if (s[i] > 64 && s[i] < 91)
+			s[i] += 32;
+		else
+			s[i] -= 32;
+	}
+}
+
+int main(void)
+{
+	char str[13] = "You can do it";
+	ft_striteri(str, ft_changecase);
+
+	printf("test: %s\n", str);
 }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: kde-paul <kde-paul@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 12:01:09 by kde-paul          #+#    #+#             */
-/*   Updated: 2025/10/29 22:10:47 by kde-paul         ###   ########.fr       */
+/*   Updated: 2025/10/30 20:06:39 by kde-paul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,30 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	str[i] = '\0';
 	return (str);
 }
-
+/* 
 #include <stdio.h>
+
+char	ft_hot13(unsigned int i, char c)
+{
+	(void)i;
+
+	if (ft_isalpha(c))
+	{
+		if ((c > 76 && c < 91) || (c > 108 && c < 123))
+			c -= 13;
+		else
+			c += 13;
+	}
+	return (c);
+}
 
 int main(void)
 {
-	char *str = "Whats goes around turns around!!";
-	char *new = ft_strmapi(str, f());
-}
+	char *str = "Rnpu cebwrpg va gur 42 Pbzzba Pber pbagnvaf na rapbqrq uvag. Sbe rnpu pvepyr, bayl bar cebwrpg cebivqrf gur pbeerpg uvag arrqrq sbe gur arkg pvepyr. Guvf punyyratr vf vaqvivqhny, jvgu n svany cevmr sbe bar fghqrag. Fgnss zrzoref znl cnegvpvcngr ohg ner abg ryvtvoyr sbe n cevmr. Ner lbh nzbat gur irel svefg gb fbyir n pvepyr? Fraq gur uvagf jvgu rkcynangvbaf gb by@42.se gb or nqqrq gb gur yrnqreobneq. Gur uvag sbe guvf svefg cebwrpg, juvpu znl pbagnva nantenzzrq jbeqf, vf: Jbys bs ntragvir cnegvpyrf gung qvfcebir terral gb lbhe ubzrf qan gung cebjfr lbhe fgbby";
+	//char *str2 = "ZZZZzzzzzz";
+	char *new = ft_strmapi(str, ft_hot13);
+
+	printf("test: %s\n", new);
+
+	free(new);
+} */
